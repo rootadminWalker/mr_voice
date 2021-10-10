08fc9dfe49aeb1edac67d0cd20bae65480bd9664
rootadminwalker <chiioleong519@gmail.com>
2021-10-10 17:24:13 +0800

    :boom: new(Hotword & Flow): Supported the hotword and flowing feature: 1, Can detect hotword 2, Can play sound effects when hotword detected 3, Can handle a flow request to re-ask the user

--- src/RespeakerNode.py
+++ src/RespeakerNode.py
@@ -1,58 +1,87 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
+import os
+import struct
+import time
+import wave
+
+import numpy as np
+import pyaudio
+import rospy
 import usb.core
 import usb.util
-import struct
 from pixel_ring import usb_pixel_ring_v2
-import time
-import pyaudio
-import numpy as np
-import wave
-import rospy
+from pydub import AudioSegment
 from std_msgs.msg import String
-import os
-
 
 PARAMETERS = {
-    'AECFREEZEONOFF': (18, 7, 'int', 1, 0, 'rw', 'Adaptive Echo Canceler updates inhibit.', '0 = Adaptation enabled', '1 = Freeze adaptation, filter only'),
+    'AECFREEZEONOFF': (18, 7, 'int', 1, 0, 'rw', 'Adaptive Echo Canceler updates inhibit.', '0 = Adaptation enabled',
+                       '1 = Freeze adaptation, filter only'),
     'AECNORM': (18, 19, 'float', 16, 0.25, 'rw', 'Limit on norm of AEC filter coefficients'),
-    'AECPATHCHANGE': (18, 25, 'int', 1, 0, 'ro', 'AEC Path Change Detection.', '0 = false (no path change detected)', '1 = true (path change detected)'),
+    'AECPATHCHANGE': (18, 25, 'int', 1, 0, 'ro', 'AEC Path Change Detection.', '0 = false (no path change detected)',
+                      '1 = true (path change detected)'),
     'RT60': (18, 26, 'float', 0.9, 0.25, 'ro', 'Current RT60 estimate in seconds'),
-    'HPFONOFF': (18, 27, 'int', 3, 0, 'rw', 'High-pass Filter on microphone signals.', '0 = OFF', '1 = ON - 70 Hz cut-off', '2 = ON - 125 Hz cut-off', '3 = ON - 180 Hz cut-off'),
+    'HPFONOFF': (
+        18, 27, 'int', 3, 0, 'rw', 'High-pass Filter on microphone signals.', '0 = OFF', '1 = ON - 70 Hz cut-off',
+        '2 = ON - 125 Hz cut-off', '3 = ON - 180 Hz cut-off'),
     'RT60ONOFF': (18, 28, 'int', 1, 0, 'rw', 'RT60 Estimation for AES. 0 = OFF 1 = ON'),
-    'AECSILENCELEVEL': (18, 30, 'float', 1, 1e-09, 'rw', 'Threshold for signal detection in AEC [-inf .. 0] dBov (Default: -80dBov = 10log10(1x10-8))'),
-    'AECSILENCEMODE': (18, 31, 'int', 1, 0, 'ro', 'AEC far-end silence detection status. ', '0 = false (signal detected) ', '1 = true (silence detected)'),
+    'AECSILENCELEVEL': (18, 30, 'float', 1, 1e-09, 'rw',
+                        'Threshold for signal detection in AEC [-inf .. 0] dBov (Default: -80dBov = 10log10(1x10-8))'),
+    'AECSILENCEMODE': (
+        18, 31, 'int', 1, 0, 'ro', 'AEC far-end silence detection status. ', '0 = false (signal detected) ',
+        '1 = true (silence detected)'),
     'AGCONOFF': (19, 0, 'int', 1, 0, 'rw', 'Automatic Gain Control. ', '0 = OFF ', '1 = ON'),
-    'AGCMAXGAIN': (19, 1, 'float', 1000, 1, 'rw', 'Maximum AGC gain factor. ', '[0 .. 60] dB (default 30dB = 20log10(31.6))'),
-    'AGCDESIREDLEVEL': (19, 2, 'float', 0.99, 1e-08, 'rw', 'Target power level of the output signal. ', '[-inf .. 0] dBov (default: -23dBov = 10log10(0.005))'),
-    'AGCGAIN': (19, 3, 'float', 1000, 1, 'rw', 'Current AGC gain factor. ', '[0 .. 60] dB (default: 0.0dB = 20log10(1.0))'),
+    'AGCMAXGAIN': (
+        19, 1, 'float', 1000, 1, 'rw', 'Maximum AGC gain factor. ', '[0 .. 60] dB (default 30dB = 20log10(31.6))'),
+    'AGCDESIREDLEVEL': (19, 2, 'float', 0.99, 1e-08, 'rw', 'Target power level of the output signal. ',
+                        '[-inf .. 0] dBov (default: -23dBov = 10log10(0.005))'),
+    'AGCGAIN': (
+        19, 3, 'float', 1000, 1, 'rw', 'Current AGC gain factor. ', '[0 .. 60] dB (default: 0.0dB = 20log10(1.0))'),
     'AGCTIME': (19, 4, 'float', 1, 0.1, 'rw', 'Ramps-up / down time-constant in seconds.'),
     'CNIONOFF': (19, 5, 'int', 1, 0, 'rw', 'Comfort Noise Insertion.', '0 = OFF', '1 = ON'),
-    'FREEZEONOFF': (19, 6, 'int', 1, 0, 'rw', 'Adaptive beamformer updates.', '0 = Adaptation enabled', '1 = Freeze adaptation, filter only'),
+    'FREEZEONOFF': (19, 6, 'int', 1, 0, 'rw', 'Adaptive beamformer updates.', '0 = Adaptation enabled',
+                    '1 = Freeze adaptation, filter only'),
     'STATNOISEONOFF': (19, 8, 'int', 1, 0, 'rw', 'Stationary noise suppression.', '0 = OFF', '1 = ON'),
     'GAMMA_NS': (19, 9, 'float', 3, 0, 'rw', 'Over-subtraction factor of stationary noise. min .. max attenuation'),
-    'MIN_NS': (19, 10, 'float', 1, 0, 'rw', 'Gain-floor for stationary noise suppression.', '[-inf .. 0] dB (default: -16dB = 20log10(0.15))'),
+    'MIN_NS': (19, 10, 'float', 1, 0, 'rw', 'Gain-floor for stationary noise suppression.',
+               '[-inf .. 0] dB (default: -16dB = 20log10(0.15))'),
     'NONSTATNOISEONOFF': (19, 11, 'int', 1, 0, 'rw', 'Non-stationary noise suppression.', '0 = OFF', '1 = ON'),
-    'GAMMA_NN': (19, 12, 'float', 3, 0, 'rw', 'Over-subtraction factor of non- stationary noise. min .. max attenuation'),
-    'MIN_NN': (19, 13, 'float', 1, 0, 'rw', 'Gain-floor for non-stationary noise suppression.', '[-inf .. 0] dB (default: -10dB = 20log10(0.3))'),
+    'GAMMA_NN': (
+        19, 12, 'float', 3, 0, 'rw', 'Over-subtraction factor of non- stationary noise. min .. max attenuation'),
+    'MIN_NN': (19, 13, 'float', 1, 0, 'rw', 'Gain-floor for non-stationary noise suppression.',
+               '[-inf .. 0] dB (default: -10dB = 20log10(0.3))'),
     'ECHOONOFF': (19, 14, 'int', 1, 0, 'rw', 'Echo suppression.', '0 = OFF', '1 = ON'),
-    'GAMMA_E': (19, 15, 'float', 3, 0, 'rw', 'Over-subtraction factor of echo (direct and early components). min .. max attenuation'),
-    'GAMMA_ETAIL': (19, 16, 'float', 3, 0, 'rw', 'Over-subtraction factor of echo (tail components). min .. max attenuation'),
+    'GAMMA_E': (19, 15, 'float', 3, 0, 'rw',
+                'Over-subtraction factor of echo (direct and early components). min .. max attenuation'),
+    'GAMMA_ETAIL': (
+        19, 16, 'float', 3, 0, 'rw', 'Over-subtraction factor of echo (tail components). min .. max attenuation'),
     'GAMMA_ENL': (19, 17, 'float', 5, 0, 'rw', 'Over-subtraction factor of non-linear echo. min .. max attenuation'),
     'NLATTENONOFF': (19, 18, 'int', 1, 0, 'rw', 'Non-Linear echo attenuation.', '0 = OFF', '1 = ON'),
-    'NLAEC_MODE': (19, 20, 'int', 2, 0, 'rw', 'Non-Linear AEC training mode.', '0 = OFF', '1 = ON - phase 1', '2 = ON - phase 2'),
-    'SPEECHDETECTED': (19, 22, 'int', 1, 0, 'ro', 'Speech detection status.', '0 = false (no speech detected)', '1 = true (speech detected)'),
-    'FSBUPDATED': (19, 23, 'int', 1, 0, 'ro', 'FSB Update Decision.', '0 = false (FSB was not updated)', '1 = true (FSB was updated)'),
-    'FSBPATHCHANGE': (19, 24, 'int', 1, 0, 'ro', 'FSB Path Change Detection.', '0 = false (no path change detected)', '1 = true (path change detected)'),
+    'NLAEC_MODE': (
+        19, 20, 'int', 2, 0, 'rw', 'Non-Linear AEC training mode.', '0 = OFF', '1 = ON - phase 1', '2 = ON - phase 2'),
+    'SPEECHDETECTED': (19, 22, 'int', 1, 0, 'ro', 'Speech detection status.', '0 = false (no speech detected)',
+                       '1 = true (speech detected)'),
+    'FSBUPDATED': (
+        19, 23, 'int', 1, 0, 'ro', 'FSB Update Decision.', '0 = false (FSB was not updated)',
+        '1 = true (FSB was updated)'),
+    'FSBPATHCHANGE': (19, 24, 'int', 1, 0, 'ro', 'FSB Path Change Detection.', '0 = false (no path change detected)',
+                      '1 = true (path change detected)'),
     'TRANSIENTONOFF': (19, 29, 'int', 1, 0, 'rw', 'Transient echo suppression.', '0 = OFF', '1 = ON'),
-    'VOICEACTIVITY': (19, 32, 'int', 1, 0, 'ro', 'VAD voice activity status.', '0 = false (no voice activity)', '1 = true (voice activity)'),
+    'VOICEACTIVITY': (19, 32, 'int', 1, 0, 'ro', 'VAD voice activity status.', '0 = false (no voice activity)',
+                      '1 = true (voice activity)'),
     'STATNOISEONOFF_SR': (19, 33, 'int', 1, 0, 'rw', 'Stationary noise suppression for ASR.', '0 = OFF', '1 = ON'),
-    'NONSTATNOISEONOFF_SR': (19, 34, 'int', 1, 0, 'rw', 'Non-stationary noise suppression for ASR.', '0 = OFF', '1 = ON'),
-    'GAMMA_NS_SR': (19, 35, 'float', 3, 0, 'rw', 'Over-subtraction factor of stationary noise for ASR. ', '[0.0 .. 3.0] (default: 1.0)'),
-    'GAMMA_NN_SR': (19, 36, 'float', 3, 0, 'rw', 'Over-subtraction factor of non-stationary noise for ASR. ', '[0.0 .. 3.0] (default: 1.1)'),
-    'MIN_NS_SR': (19, 37, 'float', 1, 0, 'rw', 'Gain-floor for stationary noise suppression for ASR.', '[-inf .. 0] dB (default: -16dB = 20log10(0.15))'),
-    'MIN_NN_SR': (19, 38, 'float', 1, 0, 'rw', 'Gain-floor for non-stationary noise suppression for ASR.', '[-inf .. 0] dB (default: -10dB = 20log10(0.3))'),
-    'GAMMAVAD_SR': (19, 39, 'float', 1000, 0, 'rw', 'Set the threshold for voice activity detection.', '[-inf .. 60] dB (default: 3.5dB 20log10(1.5))'),
+    'NONSTATNOISEONOFF_SR': (
+        19, 34, 'int', 1, 0, 'rw', 'Non-stationary noise suppression for ASR.', '0 = OFF', '1 = ON'),
+    'GAMMA_NS_SR': (19, 35, 'float', 3, 0, 'rw', 'Over-subtraction factor of stationary noise for ASR. ',
+                    '[0.0 .. 3.0] (default: 1.0)'),
+    'GAMMA_NN_SR': (19, 36, 'float', 3, 0, 'rw', 'Over-subtraction factor of non-stationary noise for ASR. ',
+                    '[0.0 .. 3.0] (default: 1.1)'),
+    'MIN_NS_SR': (19, 37, 'float', 1, 0, 'rw', 'Gain-floor for stationary noise suppression for ASR.',
+                  '[-inf .. 0] dB (default: -16dB = 20log10(0.15))'),
+    'MIN_NN_SR': (19, 38, 'float', 1, 0, 'rw', 'Gain-floor for non-stationary noise suppression for ASR.',
+                  '[-inf .. 0] dB (default: -10dB = 20log10(0.3))'),
+    'GAMMAVAD_SR': (19, 39, 'float', 1000, 0, 'rw', 'Set the threshold for voice activity detection.',
+                    '[-inf .. 60] dB (default: 3.5dB 20log10(1.5))'),
     # 'KEYWORDDETECT': (20, 0, 'int', 1, 0, 'ro', 'Keyword detected. Current value so needs polling.'),
     'DOAANGLE': (21, 0, 'int', 359, 0, 'ro', 'DOA angle. Current value. Orientation depends on build configuration.')
 }
@@ -128,7 +157,7 @@ class RespeakerInterface(object):
         if data[2] == 'int':
             result = response[0]
         else:
-            result = response[0] * (2.**response[1])
+            result = response[0] * (2. ** response[1])
 
         return result
 
@@ -142,7 +171,7 @@ class RespeakerInterface(object):
 
     def set_led_color(self, r, g, b, a):
         self.pixel_ring.set_brightness(int(20 * a))
-        self.pixel_ring.set_color(r=int(r*255), g=int(g*255), b=int(b*255))
+        self.pixel_ring.set_color(r=int(r * 255), g=int(g * 255), b=int(b * 255))
 
     def set_vad_threshold(self, db):
         self.write('GAMMAVAD_SR', db)
@@ -168,6 +197,8 @@ class RespeakerInterface(object):
 
 
 class RespeakerAudio(object):
+    # TODO: Find a way to increase the volume instead of reading the file again
+    # TODO: Get the hotword state instead of using parameters
     def __init__(self, on_audio, channels=None, suppress_error=True):
         self.on_audio = on_audio
         self.pyaudio = pyaudio.PyAudio()
@@ -214,7 +245,6 @@ class RespeakerAudio(object):
             format=pyaudio.paInt16,
             channels=self.available_channels,
             rate=self.rate,
-            frames_per_buffer=1024,
             stream_callback=self.stream_callback,
             input_device_index=self.device_index,
         )
@@ -253,7 +283,12 @@ class RespeakerAudio(object):
 
 
 def on_audio(data, channel):
-    global pub, audio_dir, respeaker_audio, respeaker_interface, max_buf, is_voice_buf, direction_buf, audio_buf
+    global pub, audio_dir, respeaker_audio, respeaker_interface, max_buf, is_voice_buf, direction_buf, audio_buf, \
+        silent_duration
+    if rospy.get_param('/voice/hotword_detected'):
+        rospy.sleep(.5)
+        rospy.set_param('/voice/hotword_detected', False)
+
     if channel == 0:
         if len(is_voice_buf) >= max_buf:
             is_voice_buf.pop(0)
@@ -271,12 +306,20 @@ def on_audio(data, channel):
 
                 print("Save to", path, "direction:", direction)
 
+                audio_buf.insert(
+                    0, np.zeros(int(silent_duration * respeaker_audio.rate)).tobytes()
+                )
+
                 wf = wave.open(path, "wb")
                 wf.setnchannels(1)
                 wf.setsampwidth(respeaker_audio.pyaudio.get_sample_size(pyaudio.paInt16))
                 wf.setframerate(respeaker_audio.rate)
                 wf.writeframes(b"".join(audio_buf))
                 wf.close()
+
+                audio = AudioSegment.from_wav(path)
+                louder = audio + 10
+                louder.export(path, format='wav')
 
                 audio_buf = []
                 pub.publish(path)
@@ -289,12 +332,13 @@ if __name__ == "__main__":
 
     max_buf = rospy.get_param("buffer_size", 10)
     audio_dir = rospy.get_param("audio_directory", "/tmp/speech")
-    
+    silent_duration = rospy.get_param("silent_duration", 0.1)
+
     pub = rospy.Publisher("~audio_path", String, queue_size=1)
 
     if not os.path.exists(audio_dir):
         os.mkdir(audio_dir)
-    
+
     is_voice_buf = []
     direction_buf = []
     audio_buf = []
@@ -302,5 +346,5 @@ if __name__ == "__main__":
     respeaker_interface = RespeakerInterface()
     respeaker_audio = RespeakerAudio(on_audio)
     respeaker_audio.start()
-    
+
     rospy.spin()
--- src/SpeechToTextNode.py
+++ src/SpeechToTextNode.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
-import speech_recognition as sr
+from os import path
 from threading import Thread
+
+import actionlib
+import numpy as np
+import pvporcupine
 import rospy
-from std_msgs.msg import String
+import soundfile
+import speech_recognition as sr
+from audioplayer import AudioPlayer
+from home_robot_msgs.msg import IntentManagerAction, IntentManagerGoal
 from mr_voice.msg import Voice
+from rospkg import RosPack
+from std_msgs.msg import String
+from std_srvs.srv import Trigger, TriggerResponse
 
 
 class SpeechToTextNode(object):
+    # TODO: Try not to modify the hotword_detected variable at flowing and processing part
     def __init__(self):
         self.topic_audio_path = rospy.get_param("topic_audio_path", "/respeaker/audio_path")
         self.topic_text = rospy.get_param("topic_text", "~text")
@@ -17,45 +28,119 @@ class SpeechToTextNode(object):
         self.pub_voice = rospy.Publisher(self.topic_text, Voice)
         self.facial = rospy.Publisher('/home_edu/facial', String, queue_size=1)
 
+        rospy.set_param("~hotword_detected", False)
+
+        rospy.Service('~start_flow', Trigger, self.start_flow_cb)
+        rospy.Service('~stop_flow', Trigger, self.stop_flow_cb)
+
+        self.intent_manager_proxy = actionlib.SimpleActionClient('/snips_intent_manager', IntentManagerAction)
+
         self.sr = sr.Recognizer()
         self.threads = []
         self.facial.publish('happy-1:Listening')
+
+        base = RosPack().get_path('mr_voice')
+        self.keywords_list = ['hey snippy', 'assistant', 'hey robie']
+        self.__hey_snippy_model = path.join(base,
+                                            'keyword_models/hey-snippy__en_linux_2021-10-30-utc_v1_9_0.ppn')
+        self.__assistant_model = path.join(base,
+                                           'keyword_models/assistant__en_linux_2021-10-30-utc_v1_9_0.ppn')
+        self.__hey_robie_model = path.join(base,
+                                           'keyword_models/hey-robie__en_linux_2021-10-30-utc_v1_9_0.ppn')
+
+        self.porcupine = pvporcupine.create(
+            library_path=pvporcupine.LIBRARY_PATH,
+            model_path=pvporcupine.MODEL_PATH,
+            keyword_paths=[
+                self.__hey_snippy_model,
+                self.__assistant_model,
+                self.__hey_robie_model
+            ]
+        )
+
+        self.hotword_detected = False
+        self.flowing = False
+
+        self.snd_wakeup = AudioPlayer(
+            path.join(base, 'snd/snd_wakeup.mp3')
+        )
+        self.snd_recognized = AudioPlayer(
+            path.join(base, 'snd/snd_recognized.mp3')
+        )
 
     def callback_audio_path(self, msg: String):
         t = Thread(target=self._recognize_thread, args=(msg.data,))
         t.start()
 
-    def _recognize_thread(self, path):
+    def start_flow_cb(self, req):
+        self.flowing = True
+        return TriggerResponse(success=True, message="Flow started")
+
+    def stop_flow_cb(self, req):
+        self.flowing = False
+        return TriggerResponse(success=True, message="Flow stopped")
+
+    def __detect_hotword(self, audio_path):
+        wav_data = soundfile.read(audio_path, dtype='int16')[0]
+        samples_count = len(wav_data) // self.porcupine.frame_length
+        for i in np.arange(samples_count):
+            frame = wav_data[i * self.porcupine.frame_length:(i + 1) * self.porcupine.frame_length]
+            result = self.porcupine.process(frame)
+            if result >= 0:
+                return result
+        return False
+
+    def _recognize_thread(self, audio_path):
         text = ""
         direction = 0
         try:
-            direction = int(path.split(".")[0].split("-")[1])
+            direction = int(audio_path.split(".")[0].split("-")[1])
         except Exception as e:
-            rospy.logerr(e.message)
+            rospy.logerr(e)
 
-        with sr.AudioFile(path) as source:
+        with sr.AudioFile(audio_path) as source:
+            if not self.hotword_detected:
+                self.hotword_detected = self.__detect_hotword(audio_path)
+                if self.hotword_detected:
+                    self.snd_wakeup.play()
+                    rospy.loginfo(f'Detected hotword {self.keywords_list[self.hotword_detected]}')
+                    return
+
+            rospy.set_param("~hotword_detected", bool(self.hotword_detected))
             audio = self.sr.record(source)
-        try:
-            self.facial.publish('smiling:Recognizing')
-            rospy.loginfo(f"recognizing file: {path}")
-            text = self.sr.recognize_google(audio, language=self.lang)
-        except sr.UnknownValueError:
-            self.facial.publish('crying:Voice Recognition could not understand audio')
-            rospy.logerr("Voice Recognition could not understand audio")
-        except sr.RequestError as e:
-            self.facial.publish('suspicious:Could not request results from Voice recognition service')
-            rospy.logerr("Could not request results from Voice Recognition service; {0}".format(e))
-        rospy.loginfo(f"{path}: {text}")
-        if len(text) > 0:
-            voice = Voice()
-            voice.time = rospy.Time.now()
-            voice.text = text
-            voice.direction = direction
-            self.pub_voice.publish(voice)
-        else:
-            rospy.logerr("nothing")
 
-        self.facial.publish('happy-1:Listening')
+        if self.hotword_detected:
+            self.snd_recognized.play()
+            try:
+                self.facial.publish('smiling:Recognizing')
+                rospy.loginfo(f"recognizing file: {audio_path}")
+                text = self.sr.recognize_google(audio, language=self.lang)
+            except sr.UnknownValueError:
+                self.facial.publish('crying:Voice Recognition could not understand audio')
+                rospy.logerr("Voice Recognition could not understand audio")
+            except sr.RequestError as e:
+                self.facial.publish('suspicious:Could not request results from Voice recognition service')
+                rospy.logerr("Could not request results from Voice Recognition service; {0}".format(e))
+            rospy.loginfo(f"{audio_path}: {text}")
+            if len(text) > 0:
+                voice = Voice()
+                voice.time = rospy.Time.now()
+                voice.text = text
+                voice.direction = direction
+                self.pub_voice.publish(voice)
+            else:
+                rospy.logerr("nothing")
+
+            self.facial.publish('happy-1:Listening')
+
+            goal = IntentManagerGoal()
+            goal.text = text
+            self.intent_manager_proxy.send_goal(goal)
+            self.intent_manager_proxy.wait_for_result()
+            if self.flowing:
+                self.snd_wakeup.play()
+            else:
+                self.hotword_detected = False
 
 
 if __name__ == "__main__":
