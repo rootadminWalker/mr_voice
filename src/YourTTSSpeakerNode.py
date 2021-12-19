46ba7b23299b2de91d00db73b27f9f30070f34b1
rootadminwalker <chiioleong519@gmail.com>
2021-12-19 15:50:10 +0800

    :new: feature(YourTTS): Speaker ROS node with YourTTS implementation

---
+++ src/YourTTSSpeakerNode.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+import rospy
+from mr_voice.srv import SpeakerSrv, SpeakerSrvResponse
+from std_msgs.msg import String
+
+from YourTTS import YourTTS
+
+
+class Speaker(object):
+    def __init__(self):
+        self.tts = YourTTS(
+            model_directory_path='/home/root_walker/workspace/ROS_projects/src/The_Essense_of_the_Grey_Region/models/yourtts',
+            reference_files='/home/root_walker/workspace/normal_projects/yourtts_test/reference_files/shannon.wav',
+            use_cuda=False
+        )
+
+        self.is_running = False
+        self.buffer = []
+
+    def on_start(self, name):
+        self.is_running = True
+
+    def on_word(self, name, location, length):
+        pass
+
+    def on_end(self, name, completed):
+        self.is_running = False
+
+    def say(self, message: str) -> bool:
+        self.buffer.append(message)
+        if not self.is_running:
+            while len(self.buffer) > 0:
+                self.tts.say(self.buffer.pop(0))
+            return False
+        return True
+
+
+class YourTTSSpeakerNode(object):
+    def __init__(self):
+        self.param_is_saying = "~is_saying"
+        self.topic_say = "~say"
+
+        self.speaker = Speaker()
+
+        rospy.set_param(self.param_is_saying, False)
+        self.facial = rospy.Publisher('/home_edu/facial', String, queue_size=1)
+        rospy.Subscriber(self.topic_say, String, self.callback_say)
+        rospy.Service('~text', SpeakerSrv, self.srv_callback)
+
+    def callback_say(self, msg: String):
+        rospy.set_param(self.param_is_saying, True)
+        self.facial.publish(f'happy-2:{msg.data}')
+        is_saying = self.speaker.say(msg.data)
+        self.facial.publish(f'happy:')
+        rospy.set_param(self.param_is_saying, is_saying)
+
+    def srv_callback(self, req):
+        rospy.set_param(self.param_is_saying, True)
+        is_saying = self.speaker.say(req.text)
+        rospy.set_param(self.param_is_saying, is_saying)
+        return SpeakerSrvResponse(True)
+
+
+if __name__ == "__main__":
+    rospy.init_node("speaker")
+    YourTTSSpeakerNode()
+    rospy.loginfo(rospy.get_name() + " OK.")
+    rospy.spin()
